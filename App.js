994ea4eceacca4397ac3e62261d18bb6ed808e3b
Jonatank28 <jonatankaleidakk28@gmail.com>
2023-06-29 19:55:21 -0300

    firt commit

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { } from 'react';
+import { Button, Text, View } from 'react-native';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+    return (
+        <View className="flex-1 items-center justify-center bg-red-100">
+            <Text className="text-4xl font-bold mb-8 text-center">
+                Welcome to your App in React Native!
+            </Text>
+        </View>
+    );
+}
